6744d6495c2893d8503a77f50037e386f0ae4933
Dhruv Modi <dhruvmodi135@gmail.com>
2020-09-28 16:24:17 -0400

    added fourier and comments

--- test2.py
+++ test2.py
@@ -16,39 +16,37 @@ if "record" is not locals():
 
 
 
-
+#just some data reading
 record = wfdb.rdrecord('rec_1')
 wfdb.plot_wfdb(record=record, title='MIT record 1')
 display(record.__dict__)
 
 
-
+#read in noisy data for 2 seconds
 signals, fields = wfdb.rdsamp('rec_1', channels=[0], sampfrom=0, sampto= 1000) #194400)
-display(signals)
-display(fields)
-
+#read in clean data for 2 seconds
 signals0, fields0 = wfdb.rdsamp('rec_1', channels=[1], sampfrom=0, sampto= 1000) #194400)
-display(signals0)
-display(fields0)
 
 
-
+#assign variables and create time-frame
 sampling_freq = 500
 sampling_duration = 2
 number_of_samples = int(sampling_freq * sampling_duration)
 time = np.linspace(0, sampling_duration, number_of_samples, endpoint=False)
 
+#plot noisy data vs clean
 plt.plot(time, signals, 'b-', label='signal')
 plt.plot(time, signals0, 'g-', linewidth=2, label='filtered signal')
 plt.show()
+#plot noise data vs clean (it's different)
 noise = signals-signals0 #unfiltered - filtered should just leave noise
 plt.plot(time, noise, 'k-')
 plt.plot(time, signals0, 'g-', linewidth=2, label='filtered signal')
 plt.show()
 
-
-N  = 3    # Filter order
-Wn = .1 #1-0.735 # Cutoff frequency
+#do and plot filter
+N  = 3 # Filter order
+Wn = .1 # Cutoff frequency bw 0 & 1
 
 b, a = signal.butter(N, Wn, 'low')
 fsignals = signal.filtfilt(b, a, signals, axis=0)
@@ -57,15 +55,14 @@ plt.plot(time, signals, 'b-', label='signal')
 plt.plot(time, fsignals-0.05, 'g-', linewidth=2, label='filtered signal') #subtracting just for visuals
 plt.show()
 
+#potential SNR calculation
 ms1 = np.mean(fsignals**2)
 ms2 = np.mean(noise**2)
 SNR = 10 * np.log(ms1/ms2)
-
 print(SNR)
 
 
-
-## fourier transform
+## Fourier Transform
 f = np.fft.fft(signals)
 plt.plot(f)
 plt.show()
