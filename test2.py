59b67a9051941ea0d918daa212c8038d4d63d56e
Dhruv Modi <dhruvmodi135@gmail.com>
2020-09-28 16:18:59 -0400

    added fourier (potentially)

--- test2.py
+++ test2.py
@@ -9,42 +9,29 @@ import librosa
 import shutil
 import posixpath
 from scipy.fft import fft, ifft
+import wfdb
 
 if "record" is not locals():
     os.chdir("/Users/dhruvmodi/Desktop/ECG-Filtering/ecg-database/Person_01/")
 
-import wfdb
 
-# Demo 1 - Read a WFDB record using the 'rdrecord' function into a wfdb.Record object.
-# Plot the signals, and show the data.
-#this reads and plots a noisy record from the MIT stress test database, along with all the information about the record/patient
-#NOTE: do not use this function if you want to further manipulate the ECG data. instead, use wfdb.rdsamp
+
+
 record = wfdb.rdrecord('rec_1')
 wfdb.plot_wfdb(record=record, title='MIT record 1')
 display(record.__dict__)
 
-#%%
 
-# Demo 2 - Read certain channels and sections of the WFDB record using the simplified 'rdsamp' function
-# which returns a numpy array and a dictionary. Show the data.®
-#this section reads a portion of the same record as above (from sample 100 to sample 15000)
-#returns a "signals" (a numpy array) and "fields"(patient and signal info)
-#note: this ecg has data from 2 channels (2 different leads), if you want data from a specific lead, you have to
-#select the correct channel that corresponds to the leads(channels start at 0, not 1)
-
-#NOTE: use this function whenever you want to work with the ECG data (not rdrecord)
 
 signals, fields = wfdb.rdsamp('rec_1', channels=[0], sampfrom=0, sampto= 1000) #194400)
 display(signals)
 display(fields)
 
-signals7, fields7 = wfdb.rdsamp('rec_1', channels=[1], sampfrom=0, sampto= 1000) #194400)
-display(signals7)
-display(fields7)
+signals0, fields0 = wfdb.rdsamp('rec_1', channels=[1], sampfrom=0, sampto= 1000) #194400)
+display(signals0)
+display(fields0)
 
-z = fft(signals)
-display(np.max(z))
-#signals = np.abs(signals)
+
 
 sampling_freq = 500
 sampling_duration = 2
@@ -52,11 +39,17 @@ number_of_samples = int(sampling_freq * sampling_duration)
 time = np.linspace(0, sampling_duration, number_of_samples, endpoint=False)
 
 plt.plot(time, signals, 'b-', label='signal')
-plt.plot(time, signals7, 'g-', linewidth=2, label='filtered signal')
+plt.plot(time, signals0, 'g-', linewidth=2, label='filtered signal')
 plt.show()
+noise = signals-signals0 #unfiltered - filtered should just leave noise
+plt.plot(time, noise, 'k-')
+plt.plot(time, signals0, 'g-', linewidth=2, label='filtered signal')
+plt.show()
+
 
 N  = 3    # Filter order
 Wn = .1 #1-0.735 # Cutoff frequency
+
 b, a = signal.butter(N, Wn, 'low')
 fsignals = signal.filtfilt(b, a, signals, axis=0)
 
@@ -64,24 +57,18 @@ plt.plot(time, signals, 'b-', label='signal')
 plt.plot(time, fsignals-0.05, 'g-', linewidth=2, label='filtered signal') #subtracting just for visuals
 plt.show()
 
-#signals = signals / 1000
-#fsignals = fsignals / 1000
-#signal7 = signals7 / 1000
-
-noise = signals-fsignals #unfiltered - filtered should just leave noise
-
-'''
-rms1 = np.sqrt(np.mean(fsignals**2))
-rms2 = np.sqrt(np.mean(noise**2))
-SNR = (rms1/rms2)**2
-'''
-
 ms1 = np.mean(fsignals**2)
 ms2 = np.mean(noise**2)
 SNR = 10 * np.log(ms1/ms2)
 
 print(SNR)
 
+
+
+## fourier transform
+f = np.fft.fft(signals)
+plt.plot(f)
+plt.show()
 
 '''
 def butter_bandpass(lowcut, highcut, fs, order=5):
